f64ea3c1ea766777757e64e810299b46be25a4fb
ViniciusCN9 <vinicius.engenhariacn@gmail.com>
2022-10-17 22:24:47 -0300

    Aula05

---
+++ POO/Aula-05/processamento.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import cv2
+
+def criarDiretorio(path):
+    diretorio = f"{path.removesuffix('.jpg')}img"
+    if not os.path.isdir(diretorio):
+        os.mkdir(diretorio)
+
+def processar(path):
+    imagem = cv2.imread(path)
+    altura = imagem.shape[0]
+    largura = imagem.shape[1]
+    canal =imagem.shape[2]
+
+    criarDiretorio(path)
+
+    vermelho = np.zeros([altura, largura, 3], dtype=np.uint8)
+    verde = np.zeros([altura, largura, 3], dtype=np.uint8)
+    azul =  np.zeros([altura, largura, 3], dtype=np.uint8)
+
+    for i in range(altura):
+        for j in range(largura):
+            pixel = imagem[i, j]
+            r = pixel[2]
+            g = pixel[1]
+            b = pixel[0]
+
+            vermelho[i, j] =[0,0,r]
+            verde[i, j] =[0,g,0]
+            azul[i, j] =[b,0,0]
+            
+    cv2.imwrite(os.path.join(path, "vermelho.png"), vermelho)
+    cv2.imwrite(os.path.join(path, "verde.png"), verde)
+    cv2.imwrite(os.path.join(path, "azul.png"), azul)
+
+caminho = "./imagens"
+arquivos = os.listdir(caminho)
+
+for arquivo in arquivos:
+    path = os.path.join(caminho, arquivo)
+    processar(path)
+    
+    
+
