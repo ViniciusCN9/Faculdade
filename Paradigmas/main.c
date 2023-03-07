4d93a72dd936d9877fac2c5535f25d3508aee4ac
ViniciusCN9 <vinicius.engenhariacn@gmail.com>
2023-03-06 22:19:25 -0300

    teste

---
+++ Paradigmas/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int opcao;
+	
+	printf("Bem vindo ao sistema do Vinicius!\n");
+	printf("=============OPCOES=============");
+	prinft("1 - Alunos");
+	prinft("2 - Professores");
+	prinft("0 - Sair");
+	scanf("%d", &opcao);
+	printf("%d", opcao);
+	
+	return 0;
+}
